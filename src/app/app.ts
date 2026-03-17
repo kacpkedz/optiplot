9a209349aeca692d6d51a41ba72854fef920fc61
kacpkedz <kacper.kedziora@interia.pl>
2026-03-17 08:28:19 +0100

    cos pogrzebane

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('optiplot');
+  objetoscDzialki: number = 0;
 }
